6dba9ebe733ec7084443f51f89b50e911f86bad6
BastianG09 <105691598+BastianG09@users.noreply.github.com>
2022-07-11 15:54:44 -0500

    Add files via upload

---
+++ TabViewProject/TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by BastianC on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
